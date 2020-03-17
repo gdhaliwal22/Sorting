13d6394acc9c67555ec82c77349853ba6a407ba9
Gurpreet Dhaliwal <gdhaliwal22@gmail.com>
2020-03-17 16:14:54 -0700

    refactored selection sort

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,10 +1,5 @@
-arr1 = [2, 6, 9, 5, 3, 10, 7, 1, 4, 8]
 # TO-DO: Complete the selection_sort() function below
-
-
 def selection_sort(arr):
-    # What does selection sort do? It repeatedly finds the smallest
-    # element in the list, then it moves it to the front
     # loop through n-1 elements
     for i in range(0, len(arr) - 1):
         minIndex = i
@@ -13,31 +8,31 @@ def selection_sort(arr):
         for j in range(i + 1, len(arr)):
             if arr[j] < arr[minIndex]:
                 minIndex = j
-        # swap
-            if minIndex != i:
-                arr[i], arr[minIndex] = arr[minIndex], arr[i]
+        if minIndex != i:
+            arr[i], arr[minIndex] = arr[minIndex], arr[i]
+        # TO-DO: swap
+
+    print(arr)
     return arr
-
-
-print(selection_sort(arr1))
-
 
 # TO-DO:  implement the Bubble Sort function below
+
+
 def bubble_sort(arr):
-    lst = len(arr)
-    # loop through array
-    for num1 in range(lst):
-        for num2 in range(lst - num1 - 1):
-            if arr[num1] < arr[num2]:
-                arr[num1], arr[num2] = arr[num2], arr[num1]
+    for i in range(0, len(arr) - 1):
+        for j in range(0, len(arr) - 1 - i):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
 
     return arr
 
 
+arr1 = [1, 5, 8, 4, 2, 9, 6, 0, 3, 7]
 print(bubble_sort(arr1))
 
-
 # STRETCH: implement the Count Sort function below
+
+
 def count_sort(arr, maximum=-1):
 
     return arr
